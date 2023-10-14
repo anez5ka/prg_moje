3db306f408cc2c104b1a2d78333caa802af6f01c
anez5ka <anezka@harvan.cz>
2023-10-14 19:14:24 +0200

    final 1410

---
+++ Deathroll (Mac)/hokuspokus/hokuspokus/Program.cs
@@ -0,0 +1,188 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+            * Jednoduchy program na procviceni podminek a cyklu (lze udelat i rekurzi).
+            * 
+            * Vytvor program, kde bude uzivatel hrat s pocitacem deathroll.
+            * Pravidla deathrollu: Prvni hrac navrhne cislo (puvodne ve wowku pocet goldu, o ktere se hraci vsadi) a "rollne" navrhnute cislo, jinak receno je to stejne,
+            * jako kdyby si hodil kostkou s tolika stenami, jako je navrhnute cislo. Prvnimu hraci "padne" nejake cislo a druhy hrac si "rollne" padnute cislo.
+            * Prohrava ten hrac, kteremu padne 1 jako prvnimu.
+            * Ukazka hry: Hraci se shodnou na cisle 1000. Prvni hrac rollne 0-1000, padne mu 920. Druhy hrac rolluje 0-920, padne mu 235 atd. atd. az jednomu z hracu padne 1
+            * a ten prohrava.
+            * 
+            * Struktura:
+            * 
+            * - nadefinuj promenne, ktere budes potrebovat po celou dobu hry, tedy aktualne rollovane cislo a stav "goldu" uzivatele i pocitace (oba zacinaji treba s 1000 goldu)
+            * 
+            * - uzivatel zada prvotni sazku, ktera musi byt maximalne tolik, kolik ma goldu
+            * 
+            * Opakuj dokud nepadne jednomu z hracu 1:
+            * {
+            *      Pokud je sude kolo:
+            *      {
+            *          - uzivatel zada hodnotu, kterou rolluje
+            *          - kontroluj, ze uzivatel zadal spravnou hodnotu
+            *          - uloz rollnute cislo
+            *          - vypis uzivateli, co rollnul
+            *      }
+            *      Pokud je liche kolo:
+            *      {
+            *          - pocitac rollne nahodne cislo mezi 0 a aktualne rollovanym cislem
+            *          - vypis uzivateli, co rollnul pocitac
+            *      }
+            * }
+            * 
+            * 
+            * - posledni hrajici hrac prohral, protoze mu padla 1 a sazku bere druhy hrac
+            * - vypis uzivateli kdo vyhral a stav goldu uzivatele i pocitace
+            * 
+            * ROZSIRENI:
+            * - umozni uzivateli opakovat deathroll dokud ma nejake goldy
+            */
+
+
+// DEKLARACE
+
+int pgold = 1000; // player's gold
+int cgold = 1000; // computers's gold
+int bestscore = 0; // nejvyssi dosazeny majetek hrace
+int bet;
+int maxbet;
+int konec = 0;
+int vitez;
+
+//UVOD
+Console.WriteLine("       ----- DEATHROLL -----");
+
+while (konec == 0)
+{
+    if (bestscore < pgold)
+    {
+        bestscore = pgold;
+    }
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("");
+    Console.WriteLine($"Tvoje zlato: {pgold}. Zlato počítače: {cgold}.");
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine($"Tvůj nejvyšší dosažený majetek: {bestscore}.");
+
+    if (pgold < cgold)
+    {
+        maxbet = pgold;
+    }
+    else
+    {
+        maxbet = cgold;
+    }
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine("");
+    Console.WriteLine($"Zvol sázku, nejvyšší možná sázka: {maxbet}.");
+    bet = Convert.ToInt32(Console.ReadLine());
+    while ((bet > maxbet) || (bet < 2))
+    {
+        if (bet > maxbet)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("");
+            Console.WriteLine($"Nejvyšší možná sázka: {maxbet}!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            bet = Convert.ToInt32(Console.ReadLine());
+
+        }
+        if (bet < 2)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("");
+            Console.WriteLine("Nejmenší možná sázka, jsou 2, zkus to znovu");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            bet = Convert.ToInt32(Console.ReadLine());
+
+        }
+    }
+
+    //HAZENI
+
+    Console.WriteLine(" ");
+    vitez = Droll(bet);
+    Console.WriteLine(" ");
+
+    if (vitez == 1)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine($"PROHRÁL JSI, přicházíš o {bet} goldu.");
+        pgold = pgold - bet;
+        cgold = cgold + bet;
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"VYHRÁL JSI, získáváš {bet} goldu.");
+        pgold = pgold + bet;
+        cgold = cgold - bet;
+
+    }
+
+    //POVERTY CHECK
+
+    if (cgold == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine("");
+        Console.WriteLine("Počítač už nemá žadné zlato.");
+        Console.WriteLine("Získavá proto 1000 pro další hru.");
+
+        cgold += 1000;
+    }
+    if (pgold == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("");
+        Console.WriteLine("GAME OVER, nemáš žádné zlato.");
+        Console.WriteLine("Získáváš 1000 pro novou hru.");
+
+        pgold += 1000;
+
+    }
+    Console.WriteLine(" ");
+
+
+    Console.ForegroundColor = ConsoleColor.DarkGray;
+    Console.WriteLine("-----------------------------------");
+
+    //DROLL METHOD
+
+    static int Droll(int n)
+    {
+        int proll = 0;
+        int croll = 0;
+        Random rnd = new Random();
+        while ((croll != 1) && (proll != 1))
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            proll = rnd.Next(1, (n + 1));
+            Console.WriteLine($"Player:    {proll} (1; {n})");
+            n = proll;
+            if (proll == 1)
+            {
+                return 1;
+            }
+            croll = rnd.Next(1, (n + 1));
+            Console.WriteLine($"Computer: {croll} (1; {n})");
+            n = croll;
+            if (croll == 1)
+            {
+                return 0;
+            }
+        }
+        return 2;
+    }
+
+
+
+
+
+}
+
+
+
+
---
+++ RockPaperScissor (Mac)/RockPapeprScissorsMac/RockPapeprScissorsMac/Program.cs
@@ -0,0 +1,182 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+            * Jednoduchy program na procviceni podminek a cyklu.
+            * 
+            * Vytvor program, kde bude uzivatel hrat s pocitacem kamen-nuzky-papir.
+            * 
+            * Struktura:
+            * 
+            * - nadefinuj promenne, ktere budes potrebovat po celou dobu hry, tedy skore obou
+            *
+            * Opakuj tolikrat, kolik kol chces hrat:
+            * {
+            *      Dokud uzivatel nezada vstup spravne:
+            *      {
+            *          - nacitej vstup od uzivatele
+            *      }
+            *      
+            *      - vygeneruj s pomoci rng.Next() nahodny vstup pocitace
+            *      
+            *      Pokud vyhral uzivatel:
+            *      {
+            *          - informuj uzivatele, ze vyhral kolo
+            *          - zvys skore uzivateli o 1
+            *      }
+            *      Pokud vyhral pocitac:
+            *      {
+            *          - informuj uzivatele, ze prohral kolo
+            *          - zvys skore pocitaci o 1
+            *      }
+            *      Pokud byla remiza:
+            *      {
+            *          - informuj uzivatele, ze doslo k remize
+            *      }
+            * }
+            * 
+            * - informuj uzivatele, jake mel skore on/a a pocitac a kdo vyhral.
+            */
+
+
+// DEKLARACE, P označuje hráče, C označuje počítač
+string p;
+int c;
+int pskore = 0;
+int cskore = 0;
+Random rnd = new Random();
+int VB = 0;  // vitezny bod, pocet bodu nutny k vyhre (hra nebude ukoncena po x kolech, ale az jeden ze souperu dosahne x bodu), zamezi se tak remize
+
+Console.WriteLine("--- KÁMEN, NŮŽKY, PAPÍR ---");
+Console.WriteLine("");
+Console.WriteLine("Zadej na kolik vítězných bodů se hraje");
+while (VB == 0)
+{
+    VB = Convert.ToInt32(Console.ReadLine());
+    if (VB <= 0)
+    {
+        Console.WriteLine("hodnota musí být větší než 0, zadej znovu");
+    }
+}
+while ((VB > cskore) && (VB > pskore))
+{
+
+    p = "x";
+
+    //* 1 = kamen, 2 = nuzky, 3 = papir
+
+    Console.WriteLine("");
+    Console.WriteLine("Zadej k (kámen), n (nůžky) nebo p (papír)");
+    p = Console.ReadLine();
+
+    if ((p == "k") || (p == "n") || (p == "p"))
+    {
+        Console.WriteLine("");
+        c = rnd.Next(1, 4);
+        if (c == 1)
+        {
+            Console.WriteLine("počítač zvolil kámen");
+            Console.WriteLine("");
+            if (p == "k")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("remíza");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else if (p == "n")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("kámen drtí nůžky, bod pro počítač");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                cskore++;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("papír balí kámen, bod pro tebe");
+                Console.ForegroundColor = ConsoleColor.White;
+                pskore++;
+            }
+
+        }
+        if (c == 2)
+        {
+            Console.WriteLine("počítač zvolil nůžky");
+            Console.WriteLine("");
+
+            if (p == "k")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("kámen drtí nůžky, bod pro hráče");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                pskore++;
+            }
+            else if (p == "n")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("remíza");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("nůžky stříhají papír, bod pro počítač");
+                Console.ForegroundColor = ConsoleColor.White;
+                cskore++;
+            }
+        }
+        if (c == 3)
+        {
+            Console.WriteLine("počítač zvolil papír");
+            Console.WriteLine("");
+
+            if (p == "k")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("papír balí kámen, bod pro počítač");
+                Console.ForegroundColor = ConsoleColor.White;
+                cskore++;
+            }
+            else if (p == "n")
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("nůžky stříhají papír, bod pro hráče");
+                Console.ForegroundColor = ConsoleColor.White;
+                pskore++;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("remíza");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+        Console.WriteLine("");
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        Console.WriteLine("---------------------");
+        Console.ForegroundColor = ConsoleColor.White;
+
+
+    }
+    else
+    {
+        Console.WriteLine("Nesprávně zadaná hodnota.");
+    }
+
+    
+}
+Console.WriteLine("");
+Console.WriteLine("");
+
+if (VB == pskore)
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine($"VYHRÁL JSI. Získal jsi {pskore} bodů, počítač získal jen {cskore}.");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine($"PROHRÁL JSI. Počítač získal {cskore} bodů, ty jsi získal jen {pskore}.");
+
+}
+Console.ReadKey();
